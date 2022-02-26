fe9b3f2f5f5bc0e5eca566fcbfde526c964d7563
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-26 05:27:46 +0000

    header_now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :confiqure_permtted_paramenters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    homes_about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    homes_about_path
+  end
+
+  protected
+
+  def configure_permitted_paramenters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  # get 'users/edit'
+  # get 'users/show'
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  root 'homes#top'
+  get 'users' => 'users#index'
+  get 'homes/about' => 'homes#about'
+  # resources :users,only:[:edit, :show]
+  resources :books,only:[:new, :index, :show, :edit]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
